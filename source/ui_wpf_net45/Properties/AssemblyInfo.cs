3e038051065db658210ef72c8e864cb49945891a
Borys Korobeinikov <korobeinikov@outlook.com>
2015-01-27 19:37:15 +0200

    added xmlnspreffix for wpf 4.5 project

--- source/ui_wpf_net45/Properties/AssemblyInfo.cs
+++ source/ui_wpf_net45/Properties/AssemblyInfo.cs
@@ -5,6 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+using System.Windows.Markup;
+
 [assembly: AssemblyTitle("Se7enRedLines.Everest.UI_WPF.NET45")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -34,3 +36,11 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: XmlnsPrefix("http://schemas.7redlines.com/xaml", "se7en")]
+[assembly: XmlnsDefinition("http://schemas.7redlines.com/xaml", "Se7enRedLines.UI")]
+[assembly: XmlnsDefinition("http://schemas.7redlines.com/xaml", "Se7enRedLines.UI.Actions")]
+[assembly: XmlnsDefinition("http://schemas.7redlines.com/xaml", "Se7enRedLines.UI.Behaviors")]
+[assembly: XmlnsDefinition("http://schemas.7redlines.com/xaml", "Se7enRedLines.UI.Command")]
+[assembly: XmlnsDefinition("http://schemas.7redlines.com/xaml", "Se7enRedLines.UI.Converters")]
+[assembly: XmlnsDefinition("http://schemas.7redlines.com/xaml", "Se7enRedLines.UI.MVVM")]
